a753d0f4e99f2408e1b74c104a63963b5e459013
Brandon Unzueta <unzueta.brandon@gmail.com>
2020-03-15 18:33:14 -0700

    adding delete routes for profiles and pullbox

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -58,4 +58,57 @@ router.post("/", auth, async (req, res) => {
   }
 });
 
+///////GET api/profile /
+///////GET ALL PROFILES/
+///////PRIVATE//////////
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+///////GET api/profile/user/:user_id /
+///////GET PROFILE BY USER ID/////////
+///////PRIVATE////////////////////////
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) return res.status(400).json({ msg: "Profile not found" });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+///////DELETE api/profile /////////////
+///////DELETE PROFILE USER AND PULLBOX/
+///////PRIVATE/////////////////////////
+router.delete("/", auth, async (req, res) => {
+  try {
+    //REMOVE PULLBOX
+    await Pullbox.findByIdAndRemove({ user: req.user.id });
+    //REMOVE PULLBOX
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //REMOVE PULLBOX
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User Deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
