7a71ac893decbbb4ca0fd34c8d032e913f42b922
Brandon Unzueta <unzueta.brandon@gmail.com>
2020-03-16 20:37:08 -0700

    adding initial action types

---
+++ client/src/actions/profile.js
@@ -0,0 +1,129 @@
+import axios from "axios";
+import { setAlert } from "./alert";
+import {
+  GET_PROFILE,
+  GET_PROFILES,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  CLEAR_PROFILE,
+  DELETE_ACCOUNT
+} from "./types";
+
+//////////////////////////////
+//GET CURRENT USER'S PROFILE//
+//////////////////////////////
+export const getCurrentProfile = () => async dispatch => {
+  try {
+    const res = await axios.get("/api/profile/me");
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+////////////////////
+//GET ALL PROFILES//
+////////////////////
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+
+  try {
+    const res = await axios.get("/api/profile");
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+/////////////////////
+//GET PROFILE BY ID//
+/////////////////////
+export const getProfileById = userId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+///////////////////////////
+//CREATE / UPDATE PROFILE//
+///////////////////////////
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const res = await axios.post("/api/profile", formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit ? "Profile Updated" : "Profile Created", "success"));
+
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (err) {
+    if (errors) {
+      erros.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//////////////////
+//DELETE ACCOUNT//
+//////////////////
+export const deleteAccount = () => async dispatch => {
+  if (window.confirm("This action CANNOT be undone, are you sure?")) {
+    try {
+      const res = await axios.delete("/api/profile/");
+
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: DELETE_ACCOUNT });
+
+      dispatch(setAlert("Account has been PERMA-DEATHED"));
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  }
+};
